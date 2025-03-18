41a140f12caad9fbe844191c02297ddc54525cfc
Nathan Felix <nathan.felix@gmail.com>
2025-03-18 16:04:27 -0600

    nathan

---
+++ backend/ScrumGroup/ScrumGroup/Controllers/ReligionController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ScrumGroup.Data;
+
+namespace ScrumGroup.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ReligionController : ControllerBase
+    {
+        private ReligionDbContext _religionContext;
+
+        public ReligionController(ReligionDbContext temp)
+        {
+            _religionContext = temp;
+        }
+
+        [HttpGet("AllReligions")]
+        public IEnumerable<Religion> GetReligions()
+        {
+            return _religionContext.Religions.ToList();
+        }
+
+        [HttpGet("AllLocations")]
+        public IEnumerable<Location> GetLocations()
+        {
+            return _religionContext.Locations.ToList();
+        }
+
+        [HttpGet("AllUsers")]
+        public IEnumerable<User> Getusers()
+        {
+            return _religionContext.Users.ToList();
+        }
+
+        [HttpGet("AllStudyGuides")]
+        public IEnumerable<StudyGuide> GetStudyGuides()
+        {
+            return _religionContext.StudyGuides.ToList();
+        }
+
+        [HttpGet("AllDiscussions")]
+        public IEnumerable<Discussion> GetDiscussions()
+        {
+            return _religionContext.Discussions.ToList();
+        }
+
+    }
+}
