973f81043394f0c5af5d30b011f4cc39a3c9cd65
beccaharrison <95371071+beccaharrison@users.noreply.github.com>
2025-03-18 15:44:34 -0600

    first

---
+++ backend/ScrumGroup/ScrumGroup/Data/Discussion.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ScrumGroup.Data;
+
+public class Discussion
+{
+    [Key]
+    public int DiscussionId { get; set; }
+    [ForeignKey("UserId")]
+    public int? UserId { get; set; }
+    public User User { get; set; }
+    [Required]
+    public DateTime TimePosted { get; set; }
+    [Required]
+    public string Response  { get; set; }
+}
