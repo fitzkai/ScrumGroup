afd08f64aa51adcb4435815b98d6c2217f092101
beccaharrison <95371071+beccaharrison@users.noreply.github.com>
2025-03-18 16:03:28 -0600

    add database

--- backend/ScrumGroup/ScrumGroup/Data/User.cs
+++ backend/ScrumGroup/ScrumGroup/Data/User.cs
@@ -1,6 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ScrumGroup.Data;
 
 public class User
 {
+    [Key]
+    [Column(Order = 0)]
+    public int UserId { get; set; }
     
+    [Key]
+    [Column(Order = 1)]
+    public int ReligionId { get; set; }
+    
+    [Required]
+    public string Username { get; set; }
+    
+    [Required]
+    public string Password { get; set; }
+    
+    public string? FirstName { get; set; }
+    
+    public string? LastName { get; set; }
+    
+    [ForeignKey("ReligionKey")]
+    public Religion? Religion { get; set; }
 }
