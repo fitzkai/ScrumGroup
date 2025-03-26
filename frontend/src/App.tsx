3280ff08c5f5e9b3ffbce0632ecfacc8886d8e7c
Mercy Fetzer <131705906+MercyFetzer7@users.noreply.github.com>
2025-03-26 14:09:59 -0600

    navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Homepage from './components/Homepage'
 import Menu from './components/Menu'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <Homepage />
-      <br />
-      <br />
-      <Menu />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/menu' element={<Menu />} />
+        </Routes>
+      </Router>
     </>
   )
 }
--- frontend/src/components/Homepage.tsx
+++ frontend/src/components/Homepage.tsx
@@ -1,5 +1,3 @@
-'use client';
-import React from 'react';
 import styles from './Homepage.module.css';
 import StatusBar from './StatusBar';
 import HomeContent from './HomeContent';
