e6c3e787409916764e1773665331be847d6b6246
beccaharrison <95371071+beccaharrison@users.noreply.github.com>
2025-04-01 20:19:49 -0600

    make everything functional except search:(

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,27 +2,25 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import DiscussionBoard from './components/DiscussionBoard';
 import HomeContent from './components/HomeContent';
-import InputDesign from './components/InputDesign';
 import Homepage from './components/Homepage';
 import Menu from './components/Menu';
 import ReligionPage from './components/ReligionPage';
 import StudyGuide from './components/StudyGuide';
 import MapSearch from './mapsearch_components/MapSearch';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchDesign from './components/SearchDesign';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<HomeContent />} />
           <Route path="/" element={<Homepage />} />
           <Route path="/map" element={<MapSearch />} />
           <Route path="/religions" element={<ReligionPage />} />
           <Route path="/studyguide/:GuideId" element={<StudyGuide />} />
           <Route path="discussion" element={<DiscussionBoard />} />
           <Route path="/menu" element={<Menu />} />
-          <Route path="/search" element={<InputDesign />} />
+          <Route path="/search" element={<SearchDesign />} />
         </Routes>
       </Router>
     </>
